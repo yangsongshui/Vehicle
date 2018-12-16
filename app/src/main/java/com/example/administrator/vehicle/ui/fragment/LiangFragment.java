44543b7c89dc18284b6c017dc8756f653681de32
yangsongshui <yang3384046@126.com>
2018-12-16 08:30:15 +0800

    12.14

--- app/src/main/java/com/example/administrator/vehicle/ui/fragment/LiangFragment.java
+++ app/src/main/java/com/example/administrator/vehicle/ui/fragment/LiangFragment.java
@@ -7,22 +7,25 @@ import android.view.View;
 
 import com.example.administrator.vehicle.R;
 import com.example.administrator.vehicle.base.BaseFragment;
+import com.example.administrator.vehicle.widget.CircleProgressBar;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LiangFragment extends BaseFragment {
+    CircleProgressBar color_progress_view;
 
     public LiangFragment() {
         // Required empty public constructor
     }
 
 
-
-
     @Override
     protected void initData(View layout, Bundle savedInstanceState) {
 
+        color_progress_view = (CircleProgressBar) layout.findViewById(R.id.color_progress_view);
+        color_progress_view.setMaxStepNum(100);
+        color_progress_view.update(50, 1000);
     }
 
     @Override
