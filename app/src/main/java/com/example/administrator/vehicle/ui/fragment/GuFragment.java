1511b5a0959a9919aaa6648743d11d44684871b6
yang3384046@126.com <qq362927657>
2018-12-20 01:26:43 +0800

    12.20

--- app/src/main/java/com/example/administrator/vehicle/ui/fragment/GuFragment.java
+++ app/src/main/java/com/example/administrator/vehicle/ui/fragment/GuFragment.java
@@ -3,12 +3,14 @@ package com.example.administrator.vehicle.ui.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.administrator.vehicle.R;
 import com.example.administrator.vehicle.base.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,11 +18,23 @@ import com.example.administrator.vehicle.base.BaseFragment;
 public class GuFragment extends BaseFragment {
 
 
+    @BindView(R.id.normal_1)
+    TextView normal1;
+    @BindView(R.id.normal_2)
+    TextView normal2;
+    @BindView(R.id.normal_3)
+    TextView normal3;
+    @BindView(R.id.normal_4)
+    TextView normal4;
+    @BindView(R.id.normal_5)
+    TextView normal5;
+    @BindView(R.id.normal_6)
+    TextView normal6;
+    @BindView(R.id.normal_7)
+    TextView normal7;
     public GuFragment() {
         // Required empty public constructor
     }
-
-
 
 
     @Override
@@ -33,4 +47,24 @@ public class GuFragment extends BaseFragment {
         return R.layout.fragment_gu;
     }
 
+
+    @OnClick({R.id.car_1, R.id.car_2, R.id.car_3, R.id.car_4, R.id.car_5, R.id.car_6, R.id.car_7})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.car_1:
+                break;
+            case R.id.car_2:
+                break;
+            case R.id.car_3:
+                break;
+            case R.id.car_4:
+                break;
+            case R.id.car_5:
+                break;
+            case R.id.car_6:
+                break;
+            case R.id.car_7:
+                break;
+        }
+    }
 }
--- app/src/main/java/com/example/administrator/vehicle/ui/fragment/JingFragment.java
+++ app/src/main/java/com/example/administrator/vehicle/ui/fragment/JingFragment.java
@@ -3,12 +3,14 @@ package com.example.administrator.vehicle.ui.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.administrator.vehicle.R;
 import com.example.administrator.vehicle.base.BaseFragment;
+
+import butterknife.BindView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,9 +18,30 @@ import com.example.administrator.vehicle.base.BaseFragment;
 public class JingFragment extends BaseFragment {
 
 
-
-
-
+    @BindView(R.id.car_warning1)
+    ImageView carWarning1;
+    @BindView(R.id.top_left)
+    TextView topLeft;
+    @BindView(R.id.left)
+    TextView left;
+    @BindView(R.id.car_warning2)
+    ImageView carWarning2;
+    @BindView(R.id.top_rig)
+    TextView topRig;
+    @BindView(R.id.right)
+    TextView right;
+    @BindView(R.id.car_warning3)
+    ImageView carWarning3;
+    @BindView(R.id.bottom_left)
+    TextView bottomLeft;
+    @BindView(R.id.bleft)
+    TextView bleft;
+    @BindView(R.id.car_warning4)
+    ImageView carWarning4;
+    @BindView(R.id.bottom_rig)
+    TextView bottomRig;
+    @BindView(R.id.bright)
+    TextView bright;
 
     @Override
     protected void initData(View layout, Bundle savedInstanceState) {
@@ -29,5 +52,6 @@ public class JingFragment extends BaseFragment {
     protected int getContentView() {
         return R.layout.fragment_jing;
     }
+
 
 }
