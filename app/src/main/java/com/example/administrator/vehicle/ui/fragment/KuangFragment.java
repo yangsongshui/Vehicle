a0993295708008c73b27a43ad29888763698ea33
yang3384046@126.com <qq362927657>
2018-12-19 18:07:12 +0800

    12.19

--- app/src/main/java/com/example/administrator/vehicle/ui/fragment/KuangFragment.java
+++ app/src/main/java/com/example/administrator/vehicle/ui/fragment/KuangFragment.java
@@ -1,22 +1,106 @@
 package com.example.administrator.vehicle.ui.fragment;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 
 import com.example.administrator.vehicle.R;
 import com.example.administrator.vehicle.base.BaseFragment;
+import com.example.administrator.vehicle.bean.TabEntity;
+import com.flyco.tablayout.CommonTabLayout;
+import com.flyco.tablayout.listener.CustomTabEntity;
+import com.flyco.tablayout.listener.OnTabSelectListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 
 
-public class KuangFragment extends BaseFragment {
+public class KuangFragment extends BaseFragment implements OnTabSelectListener {
 
-
+    @BindView(R.id.tl_4)
+    CommonTabLayout mTabLayout_4;
+    private int position=0; //当前选中的位置
+    private Fragment mFragment;//刚显示的Fragment
+    private List<Fragment> frags;
+    private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
     @Override
     protected void initData(View layout, Bundle savedInstanceState) {
+        frags = new ArrayList<>();
+        frags.add(new GuFragment());
+        frags.add(new JingFragment());
+        //设置Tab菜单
+        mTabEntities.add(new TabEntity(getActivity().getString(R.string.kuang_data)));
+        mTabEntities.add(new TabEntity(getActivity().getString(R.string.kuang_data2)));
+        mTabLayout_4.setTabData(mTabEntities);
+        mTabLayout_4.setOnTabSelectListener(this);
 
     }
 
     @Override
     protected int getContentView() {
         return R.layout.fragment_kuang;
+    }
+
+    @Override
+    public void onTabSelect(int position) {
+        //tab选中
+        this.position=position;
+        Fragment currentFragment = getFragment();
+        replaceFragment(mFragment,currentFragment);
+    }
+
+    @Override
+    public void onTabReselect(int position) {
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Fragment currentFragment = getFragment();
+        replaceFragment(mFragment,currentFragment);
+    }
+
+    /**
+     *
+     * @param lastFragment
+     * @param currentFragment
+     */
+    private void replaceFragment(Fragment lastFragment, Fragment currentFragment) {
+        FragmentManager fm =getChildFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+
+        //如果两个不相等,说明切换了Fragment
+        if(lastFragment != currentFragment){
+            mFragment = currentFragment;
+
+            //隐藏刚显示的Fragment
+            if(lastFragment != null){
+                transaction.hide(lastFragment);
+            }
+            /**
+             * 显示 或者 添加当前要显示的Fragment
+             *
+             * 如果当前要显示的Fragment没添加过 则 添加
+             * 如果当前要显示的Fragment被添加过 则 隐藏
+             */
+            if(!currentFragment.isAdded()){
+                transaction.add(R.id.kuang_fl,currentFragment).commit();
+            }else {
+                transaction.show(currentFragment).commit();
+            }
+        }
+    }
+
+    /**
+     * 根据返回到对应的Fragment
+     * @return
+     */
+    private Fragment getFragment( ) {
+        return frags.get(position);
     }
 }
--- app/src/main/java/com/example/administrator/vehicle/ui/fragment/QuFragment.java
+++ app/src/main/java/com/example/administrator/vehicle/ui/fragment/QuFragment.java
@@ -1,65 +1,84 @@
 package com.example.administrator.vehicle.ui.fragment;
 
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.view.ViewGroup;
 
-import com.bumptech.glide.Glide;
 import com.example.administrator.vehicle.R;
+import com.example.administrator.vehicle.adapter.QuAdapter;
 import com.example.administrator.vehicle.base.BaseFragment;
-import com.ocnyang.pagetransformerhelp.BannerItemBean;
-import com.ocnyang.pagetransformerhelp.BannerViewPager;
-import com.ocnyang.pagetransformerhelp.ImageLoaderInterface;
+import com.example.administrator.vehicle.bean.ItemArticle;
+import com.example.administrator.vehicle.bean.TabEntity;
+import com.flyco.tablayout.CommonTabLayout;
+import com.flyco.tablayout.listener.CustomTabEntity;
+import com.flyco.tablayout.listener.OnTabSelectListener;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class QuFragment extends BaseFragment {
-    private final int[] mData = {R.drawable.img0, R.drawable.img1, R.drawable.img2, R.drawable.img0, R.drawable.img1,R.drawable.img3, R.drawable.img4,R.drawable.img0, R.drawable.img1,};
-    @BindView(R.id.viewPager2)
-    BannerViewPager mBannerViewPager;
-    private ViewPager mViewPager1;
+public class QuFragment extends BaseFragment implements OnTabSelectListener {
+    private final int[] mData = {R.drawable.img0, R.drawable.img1, R.drawable.img2, R.drawable.img0, R.drawable.img1, R.drawable.img3, R.drawable.img4, R.drawable.img0, R.drawable.img1,};
+    @BindView(R.id.qu_rv)
+    RecyclerView quRv;
+    @BindView(R.id.tl_4)
+    CommonTabLayout mTabLayout_4;
+
+    private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
+    private RecyclerView.Adapter mAdapter;
+
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void initData(View layout, Bundle savedInstanceState) {
-
-        mBannerViewPager.setData(getViewPagerDatas(),//设置数据
-                new ImageLoaderInterface() {//设置图片加载器
-                    @Override
-                    public void displayImage(Context context, Object imgPath, ImageView imageView) {
-                        Glide.with(context).load(imgPath).into(imageView);
-                    }
-                }).setPageTransformer(null)//设置切换效果
-                .setAutoPlay(false)//设置是否自动播放
-                .setOnBannerItemClickListener(new BannerViewPager.OnBannerItemClickListener() {//设置item的监听事件
-                    @Override
-                    public void OnClickLister(View view, int currentItem) {
-                        Toast.makeText(getActivity(), "" + currentItem, Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .setHaveTitle(false);//设置是否显示标题
+        //RecyclerView初始化
+        mLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        mAdapter = new QuAdapter(getData(), getActivity());
+        quRv.setLayoutManager(mLayoutManager);
+        // 设置adapter
+        quRv.setAdapter(mAdapter);
+        //设置Tab菜单
+        mTabEntities.add(new TabEntity(getActivity().getString(R.string.qu_data)));
+        mTabEntities.add(new TabEntity(getActivity().getString(R.string.qu_data2)));
+        mTabLayout_4.setTabData(mTabEntities);
+        mTabLayout_4.setOnTabSelectListener(this);
     }
 
     @Override
     protected int getContentView() {
         return R.layout.fragment_qu;
     }
-    private List<BannerItemBean> getViewPagerDatas() {
-        List<BannerItemBean> pagerItemBeanList = new ArrayList<>(mData.length);
 
-        for (int i = 0; i < mData.length; i++) {
-            pagerItemBeanList.add(new BannerItemBean(mData[i], "欧神是世界上最帅的男人 +" + (i + 1)));
-        }
-        return pagerItemBeanList;
+    /*初始化列表数据*/
+    private ArrayList<ItemArticle> getData() {
+        ArrayList<ItemArticle> data = new ArrayList<>();
+
+        data.add(new ItemArticle(mData, "时代拉开了教练课", "我自己发", "54", "64", 1));
+        data.add(new ItemArticle(mData, "时代拉开了教练课", "我自己发", "54", "64", 0));
+        data.add(new ItemArticle(mData, "时代拉开了教练课", "我自己发", "54", "64", 1));
+        data.add(new ItemArticle(mData, "时代拉开了教练课", "我自己发", "54", "64", 0));
+        data.add(new ItemArticle(mData, "时代拉开了教练课", "我自己发", "54", "64", 1));
+        return data;
+    }
+
+
+    @Override
+    public void onTabSelect(int position) {
+        //tab选中
+    }
+
+    @Override
+    public void onTabReselect(int position) {
+
     }
 }
